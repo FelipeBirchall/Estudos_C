0fd06cdea020baf262e97011cf4527becc894515
Felipe Birchal Borges <felipebirchalborges@gmail.com>
2025-02-13 18:36:02 -0300

    .

--- EhPalindromo/main.c
+++ EhPalindromo/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#define MAX 100
+
+bool EhPalindromo(char palavra[])
+{
+    int esq = 0;
+    int dir = strlen(palavra) - 1;
+    while(esq < dir)
+    {
+        if(palavra[esq] != palavra[dir])
+        {
+            return false;
+        }
+        esq++;
+        dir--;
+    }
+    return true;
+}
 
 int main()
 {
-    printf("\nHello");
+    char palavra[MAX];
+    
+    scanf("%[^\n]" , palavra);
+
+    while(strcmp(palavra , "FIM") != 0)
+    {
+        bool resp = EhPalindromo(palavra);
+
+        if(resp){printf("SIM\n");}
+
+        else{printf("NAO\n");}
+
+        scanf(" %[^\n]" , palavra);
+    }
 
     return 0;
 }
+
+
